709a563209f7af9aa6d796ce8511b0575a590ba4
Sameera <sammadu1990@gmail.com>
2023-12-02 13:36:08 +0530

    adding new terraform files

---
+++ main.tf
@@ -0,0 +1,35 @@
+data "aws_ami" "ubuntu" {
+
+    most_recent = true
+
+    filter {
+      name = "name"
+      values = ["ubuntu/images/hvm-ssd/*22.04*"]
+    }
+
+    filter {
+      name = "virtualization_type"
+      values = "hvm"
+    }
+  
+}
+
+#Provision to ap-south-1 region
+
+provider "aws" {
+
+    region = "ap-south-1"
+  
+}
+
+resource "aws_instance" "app_server" {
+    ami = data.aws_ami.ubuntu.id
+    instance_type = "t3.micro"
+    key_name = "app-ssh-key"
+
+    tags = {
+      Name = var.ec2_name
+    }
+  
+}
+
